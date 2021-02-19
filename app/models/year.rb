6d18eba745ca831aac420e4dca59e3763dc2d3f3
masayan-06 <mdmkgift@gmail.com>
2021-02-19 18:20:34 +0900

    year_エラー対処

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,7 +1,7 @@
 class Year < ActiveHash::Base
   self.data = [
       {id: 0, name: 'year'},
-      {id: 2021, name: '21'}, {id: 2022, name: '22'}, {id: 2023, name: '23'}
+      {id: 2021, name: '21'}, {id: 2022, name: '22'}, {id: 2023, name: '23'},
       {id: 2024, name: '24'}, {id: 2025, name: '25'}, {id: 2026, name: '26'},
       {id: 2027, name: '27'}, {id: 2028, name: '28'}, {id: 2029, name: '29'},
       {id: 2030, name: '30'}, {id: 2031, name: '31'}, {id: 2032, name: '32'},
